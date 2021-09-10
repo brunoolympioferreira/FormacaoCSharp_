2d2e19f3930570a76888b41a1407ddb2404f3163
Bruno Olympio <b.olympio@reply.com>
2021-09-10 16:01:34 -0300

    Primeiro commit

---
+++ MeuApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char nome = 'b';
+            var segundaLetra = 'd';
+
+            string primeiraLetra = "Meu Texto";
+            var documento = "Novo documento";
+        }
+    }
+}
