ea4c25d202f5d5ec8f6ce2cd70f187845cdfb550
Bruno Olympio <b.olympio@reply.com>
2021-09-21 16:12:49 -0300

    Comitando 21/09 tarde

--- POO/Balta/ContentContext/module.cs
+++ POO/Balta/ContentContext/module.cs
@@ -6,12 +6,10 @@ namespace Balta.ContentContext
 
     public class Module : Base
     {
-        //Construtor
         public Module()
         {
             Lectures = new List<Lecture>(); // Inicializando a lista
         }
-        //Propriedades
         public int Order { get; set; }
         public string Title { get; set; }
         public IList<Lecture> Lectures { get; set; }
