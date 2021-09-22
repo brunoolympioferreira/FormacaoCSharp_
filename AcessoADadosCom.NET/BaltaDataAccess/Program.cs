363ca31665559986abb6625f6649365e6ef0dc6e
Bruno Olympio <b.olympio@reply.com>
2021-09-22 15:57:20 -0300

    Comitando 22/09 tarde

---
+++ AcessoADadosCom.NET/BaltaDataAccess/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BaltaDataAccess.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conexão com banco de dados SQL Server
+            const string conectionString = "Server=localhost; Database = balta; Integrated Security=SSPI";
+
+            using (var connection = new SqlConnection(conectionString))
+            {
+                var categories = connection.Query<Category>("SELECT [ID],[TITLE] FROM [Category]");
+                foreach (var category in categories)
+                {
+                    System.Console.WriteLine($"{category.Id} - {category.Title}");
+                }
+            }
+        }
+    }
+}
