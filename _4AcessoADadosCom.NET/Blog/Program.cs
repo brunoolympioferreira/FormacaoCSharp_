01253f2926f7438c07495b5da3c5866cd29ef798
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2021-09-27 08:54:17 -0300

    Comitando 27/09 manha

---
+++ _4AcessoADadosCom.NET/Blog/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Blog.Models;
+using Blog.Repositories;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost;DataBase=Blog;Integrated Security=SSPI";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+
+            ReadUsersWithRoles(connection);
+            //ReadUsers(connection);
+            // ReadRoles(connection);
+            // ReadTags(connection);
+
+
+            connection.Close();
+        }
+
+        public static void ReadUsersWithRoles(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+            var items = repository.GetWithRoles();
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Name);
+                foreach (var role in item.Roles)
+                {
+                    Console.WriteLine($" - {role.Name}");
+                }
+            }
+        }
+
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Name);
+                foreach (var role in item.Roles)
+                {
+                    Console.WriteLine($" - {role.Name}");
+                }
+            }
+        }
+
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new Repository<Role>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                System.Console.WriteLine(item.Name);
+        }
+
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                System.Console.WriteLine(item.Name);
+        }
+    }
+}
